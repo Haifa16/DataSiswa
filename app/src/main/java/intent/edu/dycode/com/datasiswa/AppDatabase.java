28c6b89f3c68df90d72c685752ab4dff4f4fc88b
Haifa16 <haifashabell@gmail.com>
2019-04-22 10:23:29 +0700

    Initial commit

---
+++ app/src/main/java/intent/edu/dycode/com/datasiswa/AppDatabase.java
@@ -0,0 +1,10 @@
+package intent.edu.dycode.com.datasiswa;
+
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {SiswaModel.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract SiswaDao userDao();
+}
---
+++ app/src/main/java/intent/edu/dycode/com/datasiswa/MyApp.java
@@ -0,0 +1,18 @@
+package intent.edu.dycode.com.datasiswa;
+
+import android.app.Application;
+
+import androidx.room.Room;
+
+
+public class MyApp extends Application {
+
+    public static AppDatabase db;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class,"siswa.db").allowMainThreadQueries().build();
+    }
+}
